33f7263372490957f1ccc31581625c364b21e421
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-04-07 18:30:18 +0900

    fix: rename custom hook file according to new rules

--- app/presentation/dashboard/dashboard.container.tsx
+++ app/presentation/dashboard/dashboard.container.tsx
@@ -1,6 +1,6 @@
 import { DashboardPresentational } from "./dashboard.presentational";
 import { dashboardViewData } from "./dashboard.vd";
-import { useDashboardState } from "./use-dashboard-state";
+import { useDashboardState } from "./dashboard.hook";
 
 export function DashboardContainer() {
   const state = useDashboardState(dashboardViewData);
